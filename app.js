265f881288f121297565c1a1bf900a41099d44bd
Seunghun Lee <hun2456@hanmail.net>
2022-08-23 22:57:34 +0900

    refactor: divided server out of app.js

--- app.js
+++ app.js
@@ -1,17 +1,14 @@
 import express from "express";
 import router from "./routers/userRouter.js";
 
-export const app = express();
+const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use('/', router)
 
-app.listen(3000, () => {
-    console.log('server running')
-});
-
 app.get('/', (req, res) => {
     res.send('hello world')
 })
 
+export default app
