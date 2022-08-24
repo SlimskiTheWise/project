af055ad20947b6777e9819a5adf09fa49dc67d0f
Seunghun Lee <hun2456@hanmail.net>
2022-08-24 20:35:23 +0900

    refactor: changed mocha chai to jest

--- tests/userController.test.js
+++ tests/userController.test.js
@@ -1,41 +1,37 @@
-import chai from "chai";
-import {app} from "./../app.js";
-import chaiHttp from "chai-http";
+import app from "./../app.js";
+import request from "supertest";
 import {config} from 'dotenv'
 config()
 
-const { expect } = chai;
-chai.use(chaiHttp);
 const user = {username: "superman", password: "1234"};
 const token = process.env.TOKEN
 
 describe('hello world', function() {
         it('/', async () => {
-            const res = await chai.request(app)
-                .get('/')
+            const res = await request(app).get('/')
                 expect(res)
         });
     });
 
 describe('login', function() {
     it('should return id and token', async () => {
-        const res = await chai.request(app)
+        const res = await request(app)
             .post('/user/login')
             .send(user)
             .set("Accept", "application/json");
-        expect(res.statusCode).equal(200);
-        expect(res.body.userId).equal("62f74907d0830e2dbb587233");
+        expect(res.statusCode).toBe(200);
+        expect(res.body.userId).toBe("62f74907d0830e2dbb587233");
     });
 });
 
 describe("GET user info", () => {
     it('should return user info', async function () {
-        const res = await chai.request(app)
+        const res = await request(app)
             .get('/user')
             .set('Authorization', `Bearer ${token}`)
-        expect('Content-Type', 'application/json')
-        expect(res.statusCode).equal(200)
-        expect(res.body.username).equal('superman')
-        expect(res.body._id).equal('62f74907d0830e2dbb587233')
+            .type('Content-Type', 'application/json')
+        expect(res.statusCode).toBe(200)
+        expect(res.body.username).toBe('superman')
+        expect(res.body._id).toBe('62f74907d0830e2dbb587233')
     });
 })
