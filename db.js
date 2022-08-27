bae62fbe17df3fb699f3e29a5d69362d90a0505a
Seunghun Lee <hun2456@hanmail.net>
2022-08-27 16:53:17 +0900

    feat: implemented memory server

--- db.js
+++ db.js
@@ -1,13 +1,21 @@
 import {MongoClient} from "mongodb";
+import {MongoMemoryServer} from "mongodb-memory-server";
 
-const client = new MongoClient("mongodb://localhost:27017", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+const {NODE_ENV} = process.env;
 
-await client.connect()
-    .then(() => console.log('db connected'))
-    .catch(e => console.error(e))
+let uri;
+/** @type {MongoMemoryServer|null} */ let memoryServer = null;
 
-const db = client.db('mall')
-export const userCollection = db.collection("user")
+if (NODE_ENV==="test") {
+    memoryServer =  await MongoMemoryServer.create();
+    uri = memoryServer.getUri();
+} else if (NODE_ENV==="local") {
+    uri = "mongodb://localhost:27017"
+}
+
+export const client = await MongoClient.connect(uri);
+
+export async function closeDB() {
+    memoryServer && await memoryServer.stop();
+    await client.close();
+}
