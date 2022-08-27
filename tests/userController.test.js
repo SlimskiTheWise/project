b018537c03b7fa6bc0272f388eb1a5cdf141eab0
Seunghun Lee <hun2456@hanmail.net>
2022-08-27 16:53:27 +0900

    refactor: refactored test code
    instead of testing on the local db, implemented memory server

--- tests/userController.test.js
+++ tests/userController.test.js
@@ -1,37 +1,55 @@
 import app from "./../app.js";
-import request from "supertest";
+import {agent} from "supertest";
+import {client, closeDB} from "../db.js";
 import {config} from 'dotenv'
 config()
+import bcrypt from "bcrypt";
 
-const user = {username: "superman", password: "1234"};
 const token = process.env.TOKEN
 
-describe('hello world', function() {
-        it('/', async () => {
-            const res = await request(app).get('/')
-                expect(res)
-        });
+let server;
+let req;
+
+const {default: user} = await import("./user.json");
+
+beforeAll(async () => {server = app.listen(3333); req = agent(server, {})})
+afterAll(async () => {await closeDB(); server.close()})
+
+async function seedUser() {
+    const collection = client.db("mall").collection("users");
+    const bcryptPassword = await bcrypt.hash(user.password, 8);
+    await collection.insertOne({
+        username: user.username,
+        password: bcryptPassword,
     });
+}
 
 describe('login', function() {
-    it('should return id and token', async () => {
-        const res = await request(app)
-            .post('/user/login')
-            .send(user)
+    it('should return username and token', async () => {
+        await seedUser()
+        const res = await req.post('/user/login')
+            .send({username: user.username, password: user.password})
             .set("Accept", "application/json");
         expect(res.statusCode).toBe(200);
-        expect(res.body.userId).toBe("62f74907d0830e2dbb587233");
+        expect(res.body.username).toBe("superman");
+        expect(res.body.token)
+    });
+
+    it('should fail if wrong user information is given', async () => { //todo complete this part
+        await seedUser()
+        const res = await req.post('/user/login')
+            .send({username: "user that doesn't exist", password: user.password})
+            .set("Accept", "application/json");
+        console.log(res.body)
     });
 });
 
 describe("GET user info", () => {
     it('should return user info', async function () {
-        const res = await request(app)
-            .get('/user')
+        const res = await req.get('/user')
             .set('Authorization', `Bearer ${token}`)
             .type('Content-Type', 'application/json')
         expect(res.statusCode).toBe(200)
         expect(res.body.username).toBe('superman')
-        expect(res.body._id).toBe('62f74907d0830e2dbb587233')
     });
 })
