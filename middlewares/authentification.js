7a07789e18671ae216bebb12d881c4cd58b502c9
Seunghun Lee <hun2456@hanmail.net>
2022-08-27 16:54:00 +0900

    refactor: refactored error messages

--- middlewares/authentification.js
+++ middlewares/authentification.js
@@ -5,7 +5,7 @@ export async function verifyToken(req, res, next) {
         const token = req.header('Authorization').replace('Bearer ', '')
         const data = req.decoded = jwt.verify(token,  process.env.JWT_SECRET);
         return next()
-    } catch (error) {
-       new Error(error)
+    } catch (e) {
+       res.send(e)
     }
 }
