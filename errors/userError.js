e6e50714a5689986cc787c6f7e6565d05fa52293
Seunghun Lee <hun2456@hanmail.net>
2022-08-27 16:51:14 +0900

    feat: created error messages for users

---
+++ errors/userError.js
@@ -0,0 +1,5 @@
+const USER_NOT_FOUND = "user not found";
+const INVALID_USER = "invalid user";
+const EXISTING_USER = "user already existing"
+
+export default {USER_NOT_FOUND, INVALID_USER, EXISTING_USER}
